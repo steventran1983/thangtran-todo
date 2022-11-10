0c3e2e2cda380fa7da0a01dfea8460696fe1b3cd
steventran1983 <baothangvn@gmail.com>
2022-11-10 00:38:37 -0600

    adding

--- src/customHook/useAuth/useAuth.jsx
+++ src/customHook/useAuth/useAuth.jsx
@@ -3,26 +3,27 @@ import { useState, useEffect } from "react";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-  const [auth, setAuth] = useState(true);
+  const [auth, setAuth] = useState(false);
+
+  const verify = async () => {
+    try {
+      const res = await axios.get(
+        "https://thang-todo.herokuapp.com/api/authen/isLoggedIn"
+      );
+      console.log("Thang Cong Tu 66666666666666666666666666");
+      console.log(res.data);
+      return res.data;
+    } catch (err) {
+      return false;
+    }
+  };
 
   useEffect(() => {
-    const verify = async () => {
-      try {
-        const res = await axios.get(
-          "https://thang-todo.herokuapp.com/api/authen/isLoggedIn"
-        );
-        console.log("Thang Cong Tu 55555555555555555555");
-        return res.data;
-      } catch (err) {
-        return false;
-      }
-    };
-
     (async () => {
       const data = await verify();
+      console.log(`day la ${data}`);
       setAuth(true);
     })();
   });
-  console.log(auth);
   return { auth };
 };
