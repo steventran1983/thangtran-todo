b61163bbd00696098cbf4c101340d1243cd5973b
steventran1983 <baothangvn@gmail.com>
2022-11-10 00:21:27 -0600

    adding

--- src/customHook/useAuth/useAuth.jsx
+++ src/customHook/useAuth/useAuth.jsx
@@ -11,17 +11,16 @@ export default () => {
         const res = await axios.get(
           "https://thang-todo.herokuapp.com/api/authen/isLoggedIn"
         );
-        console.log("Thang Cong Tu");
+        console.log("Thang Cong Tu 5555555555");
         return res.data;
       } catch (err) {
         return false;
       }
     };
 
-    (async () => {
-      const data = await verify();
-      setAuth(true);
-    })();
+    const data = verify();
+    console.log(`Thang Tran day ${data}`);
+    setAuth(DataTransfer);
   });
   console.log(auth);
   return { auth };
